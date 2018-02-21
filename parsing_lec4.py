f5650b1aa93b8d363f51deb6e65cc27200fa6fc3
babay2 <dyus81@mail.ru>
2018-02-21 21:18:05 +0300

    Add 8 lec

--- parsing_lec4.py
+++ parsing_lec4.py
@@ -62,16 +62,23 @@ def Key_Stats(gather='Total Debt/Equity (mrq)'):
                         row = sp500_df[(sp500_df['Date'] == sp500_date)]
                         sp500_value = float(row['Adj Close'])
                     stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
-                    print('цена акции:', stock_price, 'ticker:', ticker)
+                    
+                    if not starting_stock_value:
+                        starting_stock_value = stock_price
+                    if not starting_sp500_value:
+                        starting_sp500_value = sp500_value
+                        
+                    stock_p_change = ((stock_price - starting_stock_value) / starting_stock_value) * 100
+                    sp500_p_change = ((sp500_value - starting_sp500_value) / starting_sp500_value) * 100
                     
                     df = df.append({'Date': date_stamp,
                                     'Unix': unix_time,
                                     'Ticker': ticker,
                                     'DeRatio': value,
                                     'Price': stock_price,
-#                                    'stock_p_change': stock_p_change,
-                                    'S&P500': sp500_value}, ignore_index=True)
-#                                    'sp500_p_change': sp500_p_change,
+                                    'stock_p_change': stock_p_change,
+                                    'S&P500': sp500_value,
+                                    'sp500_p_change': sp500_p_change}, ignore_index=True)
 #                                    'Difference': difference,
 #                                    'Status': status}, ignore_index=True)
                 except Exception as e:
