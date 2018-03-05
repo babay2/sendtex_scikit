18ea2bd5b8f298b30b050278e5b55e1f4e0f4096
babay2 <dyus81@mail.ru>
2018-03-05 21:59:32 +0300

    add 14 lec

--- svc_analize.py
+++ svc_analize.py
@@ -8,33 +8,73 @@ Created on Sun Mar  4 21:21:41 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import svm
+from sklearn import svm, preprocessing
+from sklearn.utils import shuffle
 import pandas as pd
 from matplotlib import style
 style.use("ggplot")
 
-def Build_Data_Set(features = ['DE Ratio',
-                               'Trailing P/E']):
+FEATURES =  ['DE Ratio',
+             'Trailing P/E',
+             'Price/Sales',
+             'Price/Book',
+             'Profit Margin',
+             'Operating Margin',
+             'Return on Assets',
+             'Return on Equity',
+             'Revenue Per Share',
+             'Market Cap',
+             'Enterprise Value',
+             'Forward P/E',
+             'PEG Ratio',
+             'Enterprise Value/Revenue',
+             'Enterprise Value/EBITDA',
+             'Revenue',
+             'Gross Profit',
+             'EBITDA',
+             'Net Income Avl to Common ',
+             'Diluted EPS',
+             'Earnings Growth',
+             'Revenue Growth',
+             'Total Cash',
+             'Total Cash Per Share',
+             'Total Debt',
+             'Current Ratio',
+             'Book Value Per Share',
+             'Cash Flow',
+             'Beta',
+             'Held by Insiders',
+             'Held by Institutions',
+             'Shares Short (as of',
+             'Short Ratio',
+             'Short % of Float',
+             'Shares Short (prior ']
+
+def Build_Data_Set():
     data_df = pd.read_csv('key_stats.csv')
-    data_df = data_df[:100]
-    X = np.array(data_df[features].values)
+#    data_df = data_df[:100]
+    X = np.array(data_df[FEATURES].values)
     y = (data_df['Status'].replace('underperform', 0).replace('outperform', 1).values.tolist())
+    
+    X = preprocessing.scale(X)
+    
+    X, y = shuffle(X, y, random_state = 0)
     
     return X, y
 
 def Analysis():
+    test_size = 1000
     X, y = Build_Data_Set()
+    print(len(X))
     
     clf = svm.SVC(kernel = 'linear', C = 1.0)
-    clf.fit(X,y)
-    w = clf.coef_[0]
-    a = -w[0] / w[1]
-    xx = np.linspace(min(X[:, 0]), max(X[:, 0]))
-    yy = a * xx - clf.intercept_[0] / w[1]
+    clf.fit(X[: - test_size],y[: -test_size])
+    correct_count = 0
     
-    h0 = plt.plot(xx, yy, 'k-', label='не взвешеное деление')
-    plt.scatter(X[:,0], X[:, 1], c=y)
-    plt.legend()
-    plt.show()
+    for x in range(1, test_size+1):
+        if clf.predict(X[[-x]])[0] == y[-x]:
+            correct_count += 1
+            
+    print('Точность:', (correct_count / test_size) * 100.0)
     
 Analysis()
